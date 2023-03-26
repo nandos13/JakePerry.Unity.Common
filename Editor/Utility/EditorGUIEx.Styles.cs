f3fdd606aa7cd0f42dbd538a573333673884f218
Jake Perry <jake-perry@live.com>
2023-03-26 19:43:52 +1030

    Change EditorGUIEx.Styles methods to public

--- Editor/Utility/EditorGUIEx.Styles.cs
+++ Editor/Utility/EditorGUIEx.Styles.cs
@@ -9,7 +9,7 @@ namespace JakePerry.Unity
         /// <summary>
         /// Returns the internal 'CurrentStyles' instance.
         /// </summary>
-        internal static EditorStyles CurrentStyles => (EditorStyles)typeof(EditorStyles).GetField("s_Current", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+        public static EditorStyles CurrentStyles => (EditorStyles)typeof(EditorStyles).GetField("s_Current", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
 
         /// <summary>
         /// Obtains an editor style via reflection.
@@ -18,7 +18,7 @@ namespace JakePerry.Unity
         /// <returns>
         /// The <see cref="GUIStyle"/> stored under the given field name.
         /// </returns>
-        internal static GUIStyle GetStyle(string fieldName)
+        public static GUIStyle GetStyle(string fieldName)
         {
             return (GUIStyle)typeof(EditorStyles).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic).GetValue(CurrentStyles);
         }
