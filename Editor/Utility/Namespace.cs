ffdf7ce94406632f0aa289108920755ef941c5e5
Jake Perry <jake-perry@live.com>
2024-02-07 11:21:30 +1030

    Namespace + NamespaceCache
    Added Namespace type that represents a single namespace in the project & allows easy enumeration of nested namespace + types in the namespace.

---
+++ Editor/Utility/Namespace.cs
@@ -0,0 +1,241 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace JakePerry.Unity
+{
+    /// <summary>
+    /// Representation of a namespace.
+    /// </summary>
+    public readonly struct Namespace :
+        IEnumerable,
+        IEnumerable<Namespace>,
+        IEquatable<Namespace>
+    {
+        public struct NestedNamespaceEnumerator : IEnumerator<Namespace>, IEnumerator
+        {
+            private readonly Namespace m_namespace;
+            private int m_index;
+
+            public Namespace Current => m_namespace.GetNestedNamespace(m_index);
+
+            object IEnumerator.Current => this.Current;
+
+            internal NestedNamespaceEnumerator(Namespace @namespace)
+            {
+                m_namespace = @namespace;
+                m_index = -1;
+            }
+
+            public bool MoveNext()
+            {
+                ++m_index;
+                return m_index < m_namespace.NestedCount;
+            }
+
+            void IDisposable.Dispose() { }
+
+            void IEnumerator.Reset() { m_index = -1; }
+        }
+
+        public struct TypesEnumerator : IEnumerator<Type>, IEnumerator
+        {
+            private readonly TypeCache.TypeCollection m_types;
+            private readonly string m_namespace;
+
+            private int m_index;
+
+            public Type Current => m_types[m_index];
+
+            object IEnumerator.Current => this.Current;
+
+            internal TypesEnumerator(ref TypeCache.TypeCollection types, string @namespace)
+            {
+                m_types = types;
+                m_index = -1;
+
+                // If an empty string is specified, we should use null instead for correct comparison.
+                m_namespace = (@namespace?.Length ?? 0) == 0 ? null : @namespace;
+            }
+
+            public bool MoveNext()
+            {
+                int count = m_types.Count;
+                while ((++m_index) < count)
+                {
+                    if (StringComparer.Ordinal.Equals(Current.Namespace, m_namespace))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            void IDisposable.Dispose() { }
+
+            void IEnumerator.Reset() { m_index = -1; }
+        }
+
+        public readonly struct TypesEnumerable : IEnumerable<Type>, IEnumerable
+        {
+            private readonly string m_namespace;
+            private readonly Type m_baseType;
+
+            public TypesEnumerable(string @namespace, Type baseType)
+            {
+                m_baseType = baseType ?? throw new ArgumentNullException(nameof(baseType));
+                m_namespace = @namespace;
+            }
+
+            public TypesEnumerator GetEnumerator()
+            {
+                var types = TypeCache.GetTypesDerivedFrom(m_baseType);
+                return new TypesEnumerator(ref types, m_namespace);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+            IEnumerator<Type> IEnumerable<Type>.GetEnumerator() => this.GetEnumerator();
+        }
+
+        public readonly struct TypesWithAttributeEnumerable : IEnumerable<Type>, IEnumerable
+        {
+            private readonly string m_namespace;
+            private readonly Type m_attrType;
+
+            public TypesWithAttributeEnumerable(string @namespace, Type attrType)
+            {
+                m_attrType = attrType ?? throw new ArgumentNullException(nameof(attrType));
+                m_namespace = @namespace;
+            }
+
+            public TypesEnumerator GetEnumerator()
+            {
+                var types = TypeCache.GetTypesWithAttribute(m_attrType);
+                return new TypesEnumerator(ref types, m_namespace);
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+            IEnumerator<Type> IEnumerable<Type>.GetEnumerator() => this.GetEnumerator();
+        }
+
+        private readonly string m_name;
+        private readonly string m_full;
+        private readonly Namespace[] m_children;
+
+        /// <summary>
+        /// Indicates the number of nested namespaces.
+        /// </summary>
+        public int NestedCount => m_children?.Length ?? 0;
+
+        /// <summary>
+        /// The delimited name of this namespace. Example: when <see cref="FullName"/>
+        /// is equal to <code>System.Collections.Generic</code>
+        /// this property will return <code>Generic</code>
+        /// </summary>
+        public string Name => m_name;
+
+        /// <summary>
+        /// The full name of this namespace. Example:
+        /// <code>System.Collections.Generic</code>
+        /// </summary>
+        public string FullName => m_full;
+
+        internal Namespace(string name, string full, Namespace[] children)
+        {
+            m_name = name;
+            m_full = full;
+            m_children = children;
+        }
+
+        /// <summary>
+        /// Attempt to get a nested namespace with the given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Delimeted name of the namespace.</param>
+        /// <param name="child">Matching namespace, if one is found.</param>
+        /// <returns>
+        /// <see langword="true"/> if a match is found; Otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool TryGetNestedNamespace(ReadOnlySpan<char> name, out Namespace child)
+        {
+            if (m_children is not null)
+                foreach (var c in m_children)
+                    if (name.Equals(c.Name, StringComparison.Ordinal))
+                    {
+                        child = c;
+                        return true;
+                    }
+
+            child = default;
+            return false;
+        }
+
+        /// <inheritdoc cref="TryGetNestedNamespace(ReadOnlySpan{char}, out Namespace)"/>
+        public bool TryGetNestedNamespace(string name, out Namespace child)
+        {
+            if (m_children is not null)
+                foreach (var c in m_children)
+                    if (StringComparer.Ordinal.Equals(c.Name, name))
+                    {
+                        child = c;
+                        return true;
+                    }
+
+            child = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Get the nested namespace at <paramref name="index"/>.
+        /// </summary>
+        /// <param name="index">
+        /// Index in range [0..<see cref="NestedCount"/>]
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        public Namespace GetNestedNamespace(int index)
+        {
+            var children = m_children;
+            if (children is not null && index > -1 && index < children.Length)
+            {
+                return children[index];
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        public TypesEnumerable EnumerateTypesInNamespace(Type baseType = null)
+        {
+            return new TypesEnumerable(m_full, baseType ?? typeof(object));
+        }
+
+        public TypesWithAttributeEnumerable EnumerateTypesWithAttributeInNamespace(Type attrType)
+        {
+            return new TypesWithAttributeEnumerable(m_full, attrType);
+        }
+
+        public NestedNamespaceEnumerator GetEnumerator()
+        {
+            return new NestedNamespaceEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        IEnumerator<Namespace> IEnumerable<Namespace>.GetEnumerator() => this.GetEnumerator();
+
+        public bool Equals(Namespace other)
+        {
+            return StringComparer.Ordinal.Equals(m_full, other.m_full);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Namespace other && this.Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return StringComparer.Ordinal.GetHashCode(m_full);
+        }
+    }
+}
---
+++ Editor/Utility/NamespaceCache.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace JakePerry.Unity
+{
+    /// <summary>
+    /// Provides methods for retrieving namespaces from assemblies
+    /// loaded into the Unity domain.
+    /// </summary>
+    public static class NamespaceCache
+    {
+        private struct NamespaceData
+        {
+            public string thisNamespace;
+            public string fullNamespace;
+        }
+
+        private static Namespace _root;
+        private static bool _dirty = true;
+
+        [DidReloadScripts]
+        [InitializeOnLoadMethod]
+        private static void ClearCache()
+        {
+            _dirty = true;
+            _root = default;
+        }
+
+        private static bool TryFindMatch(List<NamespaceData> list, ReadOnlySpan<char> span, out NamespaceData match)
+        {
+            foreach (var n in list)
+            {
+                if (span.Equals(n.thisNamespace.AsSpan(), StringComparison.Ordinal))
+                {
+                    match = n;
+                    return true;
+                }
+            }
+
+            match = default;
+            return false;
+        }
+
+        private static Namespace ConsolidateData(Dictionary<string, List<NamespaceData>> dict, NamespaceData data)
+        {
+            List<Namespace> children = null;
+            if (dict.TryGetValue(data.fullNamespace, out var list))
+            {
+                children = new List<Namespace>(capacity: list.Count);
+                foreach (var c in list)
+                {
+                    if (dict.ContainsKey(c.fullNamespace))
+                    {
+                        children.Add(ConsolidateData(dict, c));
+                    }
+                    else
+                    {
+                        children.Add(new Namespace(c.thisNamespace, c.fullNamespace, Array.Empty<Namespace>()));
+                    }
+                }
+            }
+
+            return new Namespace(data.thisNamespace, data.fullNamespace, children?.ToArray() ?? Array.Empty<Namespace>());
+        }
+
+        private static int CompareData(NamespaceData x, NamespaceData y)
+        {
+            return StringComparer.Ordinal.Compare(x.thisNamespace, y.thisNamespace);
+        }
+
+        private static void BuildCacheIfRequired()
+        {
+            if (!_dirty) return;
+
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            UnityEngine.Profiling.Profiler.BeginSample("### Build cache");
+
+            var dict = new Dictionary<string, List<NamespaceData>>(StringComparer.Ordinal);
+
+            foreach (var t in TypeCache.GetTypesDerivedFrom(typeof(object)))
+            {
+                string @namespace = t.Namespace;
+
+                if (string.IsNullOrEmpty(@namespace))
+                {
+                    continue;
+                }
+
+                if (dict.ContainsKey(@namespace))
+                {
+                    continue;
+                }
+
+                var root = new NamespaceData { thisNamespace = string.Empty, fullNamespace = string.Empty };
+
+                int segmentStart = 0;
+                while (segmentStart < @namespace.Length)
+                {
+                    int nextPeriodChar = @namespace.IndexOf('.', segmentStart);
+                    int segmentEnd = (nextPeriodChar < 0) ? @namespace.Length : nextPeriodChar;
+
+                    int segmentLength = segmentEnd - segmentStart;
+                    var span = @namespace.AsSpan(segmentStart, segmentLength);
+
+                    if (!dict.TryGetValue(root.fullNamespace, out List<NamespaceData> list))
+                    {
+                        list = new(capacity: 8);
+                        dict[root.fullNamespace] = list;
+                    }
+
+                    if (!TryFindMatch(list, span, out NamespaceData match))
+                    {
+                        string thisNamespace = span.ToString();
+                        string fullNamespace;
+
+                        if (root.fullNamespace.Length == 0)
+                        {
+                            fullNamespace = thisNamespace;
+                        }
+                        else if (nextPeriodChar > -1)
+                        {
+                            fullNamespace = @namespace.Substring(0, nextPeriodChar);
+                        }
+                        else
+                        {
+                            fullNamespace = @namespace;
+                        }
+
+                        match = new NamespaceData { thisNamespace = thisNamespace, fullNamespace = fullNamespace };
+                        list.Add(match);
+                    }
+
+                    root = match;
+
+                    segmentStart = segmentEnd + 1;
+                }
+            }
+
+            foreach (var l in dict.Values)
+            {
+                l.Sort(comparison: CompareData);
+            }
+
+            _root = ConsolidateData(dict, new NamespaceData { thisNamespace = string.Empty, fullNamespace = string.Empty });
+
+            _dirty = false;
+
+            // TODO: Remove this after some optimization
+            UnityEngine.Profiling.Profiler.EndSample();
+            sw.Stop();
+            UnityEngine.Debug.LogError($"Built namespace cache in {sw.Elapsed.TotalMilliseconds}ms");
+        }
+
+        public static Namespace GetGlobalNamespace()
+        {
+            BuildCacheIfRequired();
+            return _root;
+        }
+
+        public static bool TryGetNamespace(string name, out Namespace @namespace)
+        {
+            BuildCacheIfRequired();
+
+            @namespace = _root;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                @namespace = _root;
+                return true;
+            }
+
+            int segmentStart = 0;
+            while (segmentStart < name.Length)
+            {
+                int nextPeriodChar = name.IndexOf('.', segmentStart);
+                int segmentEnd = (nextPeriodChar < 0) ? name.Length : nextPeriodChar;
+
+                int segmentLength = segmentEnd - segmentStart;
+                var span = name.AsSpan(segmentStart, segmentLength);
+
+                if (!@namespace.TryGetNestedNamespace(span, out Namespace next))
+                {
+                    return false;
+                }
+
+                @namespace = next;
+            }
+
+            return true;
+        }
+    }
+}
